a36dd6556ee970b33d8304ae556d0f45016cef95
Aden-Hang <f74046137@ncku.edu.tw>
2018-01-03 21:08:47 +0800

    finish

--- app.py
+++ app.py
@@ -7,40 +7,109 @@ from flask import Flask, request, send_file
 from fsm import TocMachine
 
 
-API_TOKEN = 'Your Telegram API Token'
-WEBHOOK_URL = 'Your Webhook URL'
+API_TOKEN = "545376328:AAGJI0a5beqjQjyutQ3Va_9XpnHYE6FG9sQ"
+WEBHOOK_URL = 'https://a998cfa9.ngrok.io/hook'
 
 app = Flask(__name__)
 bot = telegram.Bot(token=API_TOKEN)
 machine = TocMachine(
     states=[
-        'user',
-        'state1',
-        'state2'
+        'start',
+        'hi',
+        'hello',
+        'goodmorning',
+        'sleep',
+        'plan',
+        'no_plan',
+        'study',
+        'n_d',
+        'n_o',
+        's_d',
+        's_o',
     ],
     transitions=[
         {
             'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state1',
-            'conditions': 'is_going_to_state1'
+            'source': 'start',
+            'dest': 'hi',
+            'conditions': 'is_going_to_hi'
         },
         {
             'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state2',
-            'conditions': 'is_going_to_state2'
+            'source': 'start',
+            'dest': 'hello',
+            'conditions': 'is_going_to_hello'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'start',
+            'dest': 'goodmorning',
+            'conditions': 'is_going_to_goodmorning'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'plan',
+            'dest': 'sleep',
+            'conditions': 'is_going_to_sleep'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'plan',
+            'dest': 'no_plan',
+            'conditions': 'is_going_to_no_plan'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'plan',
+            'dest': 'study',
+            'conditions': 'is_going_to_study'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'no_plan',
+            'dest': 'n_d',
+            'conditions': 'is_going_to_n_d'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'no_plan',
+            'dest': 'n_o',
+            'conditions': 'is_going_to_n_o'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'study',
+            'dest': 's_d',
+            'conditions': 'is_going_to_s_d'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'study',
+            'dest': 's_o',
+            'conditions': 'is_going_to_s_o'
+        },
+        {
+            'trigger': 'go_plan',
+            'source': [
+                'hello',
+                'hi',
+                'goodmorning',
+                'n_d',
+                's_d'
+            ],
+            'dest': 'plan'
         },
         {
             'trigger': 'go_back',
-            'source': [
-                'state1',
-                'state2'
+            'source':  [
+                'sleep',
+                's_o',
+                'n_o',
             ],
-            'dest': 'user'
+            'dest': 'start'
         }
     ],
-    initial='user',
+    initial='start',
     auto_transitions=False,
     show_conditions=True,
 )
--- fsm.py
+++ fsm.py
@@ -8,24 +8,116 @@ class TocMachine(GraphMachine):
             **machine_configs
         )
 
-    def is_going_to_state1(self, update):
+    def is_going_to_hi(self, update):
         text = update.message.text
-        return text.lower() == 'go to state1'
+        return text.lower() == 'hi'
 
-    def is_going_to_state2(self, update):
+    def is_going_to_hello(self, update):
         text = update.message.text
-        return text.lower() == 'go to state2'
+        return text.lower() == 'hello'
 
-    def on_enter_state1(self, update):
-        update.message.reply_text("I'm entering state1")
+    def is_going_to_goodmorning(self, update):
+        text = update.message.text
+        return text.lower() == 'goodmorning'
+
+    def is_going_to_sleep(self, update):
+        text = update.message.text
+        return text.lower() == 'i am going to go to sleep'
+
+    def is_going_to_no_plan(self, update):
+        text = update.message.text
+        return text.lower() == 'i have no plan'
+
+    def is_going_to_study(self, update):
+        text = update.message.text
+        return text.lower() == 'i am going to study'
+
+    def is_going_to_n_d(self, update):
+        text = update.message.text
+        return text.lower() == 'outside is raining, i prefer to stay inside'
+
+    def is_going_to_n_o(self, update):
+        text = update.message.text
+        return text.lower() == 'ok, see you soon'
+
+    def is_going_to_s_d(self, update):
+        text = update.message.text
+        return text.lower() == 'no, i do not like to read comic books'
+
+    def is_going_to_s_o(self, update):
+        text = update.message.text
+        return text.lower() == 'good idea'
+
+    def on_enter_hi(self, update):
+        update.message.reply_text("Hi! What are the plans for today?")
+        update.message.reply_photo(open("img/blackman_question_mark.jpg","rb"))
+        self.go_plan(update)
+
+    def on_exit_hi(self, update):
+        print('Leaving hi')
+
+    def on_enter_hello(self, update):
+        update.message.reply_text("Hello! What are the plans for today?")
+        update.message.reply_photo(open("img/blackman_question_mark.jpg","rb"))
+        self.go_plan(update)
+
+    def on_exit_hello(self, update):
+        print('Leaving hello')
+
+    def on_enter_goodmorning(self, update):
+        update.message.reply_text("Goodmorning! What are the plans for today?")
+        update.message.reply_photo(open("img/blackman_question_mark.jpg","rb"))
+        self.go_plan(update)
+
+    def on_exit_goodmorning(self, update):
+        print('Leaving goodmorning')
+
+    def on_enter_sleep(self, update):
+        update.message.reply_text("Alright, goodbye")
         self.go_back(update)
 
-    def on_exit_state1(self, update):
-        print('Leaving state1')
+    def on_exit_sleep(self, update):
+        print('Leaving hi')
 
-    def on_enter_state2(self, update):
-        update.message.reply_text("I'm entering state2")
+    def on_enter_no_plan(self, update):
+        update.message.reply_text("How about go outside and have fun with friends?")
+
+    def on_exit_no_plan(self, update):
+        print('Leaving no_plan')
+
+    def on_enter_study(self, update):
+        update.message.reply_text("Are you kidding me!?")
+        update.message.reply_text("There is no word call \"study\" in your dictionary><")
+        update.message.reply_photo(open("img/laugh.jpg","rb"))
+        update.message.reply_text("Do you want to get some comics for yourself?")
+
+    def on_exit_study(self, update):
+        print('Leaving study')
+
+    def on_enter_n_d(self, update):
+        update.message.reply_text("So, what do you want to do?")
+        self.go_plan(update)
+
+    def on_exit_n_d(self, update):
+        print('Leaving n_d')
+
+    def on_enter_n_o(self, update):
+        update.message.reply_text("Have fun! See you soon~")
         self.go_back(update)
 
-    def on_exit_state2(self, update):
-        print('Leaving state2')
+    def on_exit_n_o(self, update):
+        print('Leaving n_o')
+
+    def on_enter_s_d(self, update):
+        update.message.reply_text("So, what do you want to do")
+        self.go_plan(update)
+
+    def on_exit_s_d(self, update):
+        print('Leaving s_d')
+
+    def on_enter_s_o(self, update):
+        update.message.reply_text("You can get them on the third floor")
+        self.go_back(update)
+
+    def on_exit_s_o(self, update):
+        print('Leaving s_o')
